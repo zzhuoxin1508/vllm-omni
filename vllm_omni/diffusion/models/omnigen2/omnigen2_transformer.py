be8d3ee7f1e484e01751ff6b3d5ce091cd79b88c
汪志鹏 <wangzhipeng628@gmail.com>
2026-03-22 13:00:21 +0800

    Import RMSNorm in omnigen2_transformer.py
    Signed-off-by: 汪志鹏 <wangzhipeng628@gmail.com>

--- vllm_omni/diffusion/models/omnigen2/omnigen2_transformer.py
+++ vllm_omni/diffusion/models/omnigen2/omnigen2_transformer.py
@@ -9,8 +9,8 @@ from diffusers.models.activations import get_activation
 from diffusers.models.embeddings import Timesteps, get_1d_rotary_pos_embed
 from diffusers.models.modeling_outputs import Transformer2DModelOutput
 from einops import rearrange, repeat
-from vllm.model_executor.layers.layernorm import RMSNorm
 from vllm.model_executor.layers.activation import get_act_and_mul_fn
+from vllm.model_executor.layers.layernorm import RMSNorm
 from vllm.model_executor.layers.linear import (
     MergedColumnParallelLinear,
     QKVParallelLinear,
